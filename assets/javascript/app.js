5d24fa2adb83fc68926195800faf70d0588ebb68
Nikita Mehata <nikitamehata23@gmail.com>
2019-06-14 00:50:37 -0500

    train scheduler

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,96 +1,122 @@
-//set page refresh
-setTimeout(function() {
-    window.location.href = window.location;
-  }, 60000);
-  
-  // Your web app's Firebase configuration
+
+// Global Variables
+var name;
+var destination;
+var firstArrival;
+var frequency;
+var minAway;
+var database;
+var trainFirebaseData;
+var newFirebaseData;
+var time;
+var clock;
+$(document).ready(function () {
+
+
+    // the timer at the top
+    function runningClock() {
+        time = moment().format("hh:mm:ss A");
+        $("#time").text(time);
+    }
+    //  Call function with setInterval
+    clock = setInterval(runningClock , 1000);
+ 
+         // Your web app's Firebase configuration
   var firebaseConfig = {
-      apiKey: "AIzaSyAEmjmPYxDXloD7O7Cm9pqJ2yJoXetwFLM",
-      authDomain: "trainassignment-afbef.firebaseapp.com",
-      databaseURL: "https://trainassignment-afbef.firebaseio.com",
-      projectId: "trainassignment-afbef",
-      storageBucket: "trainassignment-afbef.appspot.com",
-      messagingSenderId: "672292163021",
-      appId: "1:672292163021:web:9cf0d865bf07841f"
+    apiKey: "AIzaSyAowoj5m78XUKJUi_xLBcccTlzIYqbRosA",
+    authDomain: "trainscheduler-619b5.firebaseapp.com",
+    databaseURL: "https://trainscheduler-619b5.firebaseio.com",
+    projectId: "trainscheduler-619b5",
+    storageBucket: "trainscheduler-619b5.appspot.com",
+    messagingSenderId: "614612854656",
+    appId: "1:614612854656:web:14cb857bac896e58"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  
-  
-      // the timer at the top
-      function runningClock() {
-              time = moment().format("hh:mm:ss A");
-              $("#time").text(time);
-      }
-      //  Call function with setInterval
-      clock = setInterval(runningClock , 1000);
-  
-  //variable for database
-  database = firebase.database();
-  
-  //initial values
-  var name = "";
-  var destination = "";
-  var time = "";
-  var frequency = "";
-  
-  //display current time & update every second
-  // function displaytime(){
-  //   intervalId = setInterval ($(".current-time").text(moment().format("HH mm a")), 1000)
-  // };
-  // displaytime();
-  
-  // Capture Button Click
-  $("#add-train").on("click", function (event) {
-    event.preventDefault();
-  
-    // Grabbed values from text boxes
-    name = $("#name").val().trim();
-    destination = $("#destination").val().trim();
-    time = $("#time").val().trim();
-    frequency = parseInt($("#frequency").val().trim());
-  
-    if(name === "" || destination === "" || time === "" || frequency === ""){
-      return;
-    }
-  
-    // Code for handling the push
-    database.ref().push({
-      name: name,
-      destination: destination,
-      time: time,
-      frequency: frequency,
-      dateAdded: firebase.database.ServerValue.TIMESTAMP
+
+    // Initialize Firebase
+    var firebaseConfig =
+
+    database = firebase.database();
+
+    $("#submitButton").on("click", function (event) {
+    
+        event.preventDefault();
+
+        //  Grab Input values IF the conditon above is true
+        name = $("#trainNameInput").val().trim();
+        destination = $("#destinationInput").val().trim();
+        firstArrival = $("#firstTrainTimeInput").val().trim();
+        frequency = $("#frequencyInput").val().trim();
+        
+        // var document = document.getElementById("SubmitButton");
+        
+        console.log(firstArrival);
+
+
+        //  Link and assign variable for firebase
+            trainFirebaseData = {
+            DatatrainName: name,
+            Datadest: destination,
+            DatafirstArrival: firstArrival,
+            Datafrequency: frequency,
+            DataminAyay:minAway,
+            TimeStamp: firebase.database.ServerValue.TIMESTAMP
+        };
+        console.log(name);
+        console.log(destination);
+        console.log(firstArrival);
+        console.log(frequency);
+        console.log(minAway);
+
+        //    Variable for firebase to link train easier
+        database.ref().push(trainFirebaseData);
+
+    //  Make sure fields are back to blank after adding a train
+        clear();
+
     });
-  
-    $("#name").val("");
-    $("#destination").val("")
-    $("#time").val("")
-    $("#frequency").val("")
-  
-  });
-  
-  // Firebase watcher .on("child_added"
-  database.ref().on("child_added", function (snapshot) {
-    // storing the snapshot.val() in a variable for convenience
-    var sv = snapshot.val();
-  
-    //variables and calculations for train arrival and minutes to arrival
-    var trainFrequency = (sv.frequency);
-    var firstTime = (sv.time);
-    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-    var different = moment().diff(moment(firstTimeConverted), "minutes");
-    var timeRemaining = different % trainFrequency;
-    var minAway = trainFrequency - timeRemaining;
-    var nextArrival = moment().add(minAway, "minutes").format("MMM Do HH:mm a");
-    // Console.loging the last user's data
-    console.log(sv.name);
-    console.log(sv.destination);
-    console.log(sv.time);
-    console.log(sv.frequency);
-  
-    //change html text to display train info
-    var row = $("<tr>").append($("<td class='nameCell'>").text(sv.name), $("<td class='destinationCell'>").text(sv.destination), $("<td class='frequencyCell'>").text(sv.frequency), $("<td class='arrivalCell'>").text(nextArrival), $("<td class='minCell'>").text(minAway));
-    $("#all-display").append(row);
-  });
-  
+    database.ref().on("child_added", function (childSnapshot) {
+        //  make variable to ease reference
+        var snapName = childSnapshot.val().DatatrainName;
+        var snapDest = childSnapshot.val().Datadest;
+        var snapFreq = childSnapshot.val().Datafrequency;
+        var snapArrival = childSnapshot.val().DatafirstArrival;
+
+       
+        //  Current Time
+        var currentTime = moment();
+        console.log(currentTime);
+        //  Convert Time and configure for Future use by pushing firstArrival back 1 year
+        var firstArrivalConverted = moment(snapArrival , "HH:mm A").subtract(1, "years");
+        console.log(firstArrivalconverted);
+        //  Calculate now vs First Arrival
+        var diff = moment().diff(moment(firstArrivalConverted) , "minutes");
+        console.log(diff);
+
+        var left = diff % snapFreq;
+        //  How long till train
+        var timeLeft = snapFreq - left;
+        var newArrival = moment().add(timeLeft , "m").format("HH:mm: A");
+
+
+        $("#table-info").append("<tr><td>" + snapName +"</td><td>" + snapDest + "</td><td>" + snapFreq + "</td><td>" +
+               newArrival + "</td><td>" + timeLeft + "</td></tr>");
+
+
+    });
+
+    function clear() {
+        $("#trainNameInput").val("");
+        $("#destinationInput").val("");
+        $("#firstTrainTimeInput").val("");
+        $("#frequencyInput").val("");
+
+        
+    }
+
+
+
+});
+
+
